52690e163250b07c8773cbde009512913ddc6ffa
Leonardo Rosa <leorosawork@gmail.com>
2023-04-23 10:58:11 -0300

    "Exercício 1"

---
+++ ImprimirNumero.java
@@ -0,0 +1,13 @@
+public class ImprimirNumero{
+    public static void main(String[] args){
+        int i = 0;
+        while(i <= 10){
+            System.out.println(i);
+            i++;
+        }
+        
+            
+        
+
+    }
+}
