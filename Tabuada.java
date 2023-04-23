17548abe571ad7c47f5dbed1691aab8235ae6ca5
Leonardo Rosa <leorosawork@gmail.com>
2023-04-23 11:52:32 -0300

    "Exercício 4"

---
+++ Tabuada.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite o número que deseja saber a tabuada: ");
+        int num  = input.nextInt();
+        int i = 0;
+        
+        while(i <= 10){
+            System.out.println(num + " * " + i + " = " + i * num);
+            i++;
+        }
+
+
+
+    }
+}
